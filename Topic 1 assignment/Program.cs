01b6b7b412ae1c6049999d5b3b695b30436b5eb0
Kylie Banman <339780520@GoTVDSB.ca>
2025-02-04 14:28:51 -0500

    Changed comment to be multiple lines

--- Topic 1 assignment/Program.cs
+++ Topic 1 assignment/Program.cs
@@ -7,7 +7,9 @@
         {
             Console.WriteLine("Hello, my  name is Kylie");
             Console.WriteLine("");
-            //The following three lines are a multi-line explination of how comments work and why they might be used
+            /*The following three lines are a multi-line explination of 
+             how comments work and why they might be used. This comment is across multiple lines
+             for visibility*/
             Console.WriteLine("Comments are used to denote things that the programmer does not wish to show up in code");
             Console.WriteLine("This in on a seperate line so I can see it. Comments are denoted by two slashes (//)");
             Console.WriteLine("Comments may be used to explain sections of code or to give credit to the original author of code");
