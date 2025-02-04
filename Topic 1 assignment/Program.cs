790fdfad928db9b05920521e90887160c5a977c9
Kylie Banman <339780520@GoTVDSB.ca>
2025-02-04 14:26:57 -0500

    Started assignment

--- Topic 1 assignment/Program.cs
+++ Topic 1 assignment/Program.cs
@@ -3,8 +3,22 @@
     internal class Program
     {
         static void Main(string[] args)
+            //Kylie Banman
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, my  name is Kylie");
+            Console.WriteLine("");
+            //The following three lines are a multi-line explination of how comments work and why they might be used
+            Console.WriteLine("Comments are used to denote things that the programmer does not wish to show up in code");
+            Console.WriteLine("This in on a seperate line so I can see it. Comments are denoted by two slashes (//)");
+            Console.WriteLine("Comments may be used to explain sections of code or to give credit to the original author of code");
+            Console.WriteLine("");
+            Console.WriteLine("K   K   BBBB");
+            Console.WriteLine("K  K    B   B");
+            Console.WriteLine("K K     B   B");
+            Console.WriteLine("KK      BBBB");
+            Console.WriteLine("K K     B   B");
+            Console.WriteLine("K  K    B   B");
+            Console.WriteLine("K   K   BBBB");
         }
     }
 }
